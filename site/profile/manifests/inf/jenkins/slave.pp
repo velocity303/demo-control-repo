79e7b336db702cca5929120b2d2af63b383fa948
James E. Jones <james.jones@puppet.com>
2016-12-12 12:36:39 -0600

    add credentials to jenkins

--- site/profile/manifests/inf/jenkins/slave.pp
+++ site/profile/manifests/inf/jenkins/slave.pp
@@ -2,5 +2,7 @@ class profile::inf::jenkins::slave {
   class { 'jenkins::slave':
     masterurl => 'http://jenkins02.infrastructure.lab:8080',
     version   => '2.2',
+    ui_user   => 'admin',
+    ui_pass   => 'puppetlabs',
   }
 }
