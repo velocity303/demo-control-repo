d881d5a3cd30de1f97f7ea9ee7b30aea166b04c0
James E. Jones <james.jones@puppet.com>
2016-12-14 23:21:42 -0600

    update puppet

--- site/profile/manifests/app/myapp.pp
+++ site/profile/manifests/app/myapp.pp
@@ -9,4 +9,30 @@ class profile::app::myapp (
     priority => 99,
     gpgcheck => 0
   }
+  if !defined(Package['unzip']) {
+    package {'unzip': ensure => present, }
+  }
+
+  $doc_root = '/opt/myapp/'
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  file { $doc_root:
+    ensure => directory,
+    owner  => 'apache',
+    group  => 'apache',,
+    mode   => '0755',
+  }
+
+  apache::vhost { $::fqdn:
+    port    => '80',
+    docroot => $doc_root,
+    require => File[$doc_root],
+  }
+  
+  package { 'myapp':
+    ensure => hiera('myapp-build-version'),
+  }
+
 }
