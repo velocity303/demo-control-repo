551460617e9e5de29f4d2f81239990c05917d73b
James E. Jones <james.jones@puppet.com>
2017-01-18 23:14:41 -0600

    update tomcat code versions

--- site/profile/manifests/app/plsample.pp
+++ site/profile/manifests/app/plsample.pp
@@ -6,17 +6,17 @@ class profile::app::plsample (
 
   case $tomcat_major_version {
     '6': {
-      $tomcat_version = '6.0.44'
+      $tomcat_version = '6.0.48'
       $catalina_dir = '/opt/apache-tomcat6'
       $tomcat_other_versions = [ '7', '8']
     }
     '7': {
-      $tomcat_version = '7.0.64'
+      $tomcat_version = '7.0.73'
       $catalina_dir = '/opt/apache-tomcat7'
       $tomcat_other_versions = [ '6', '8']
     }
     '8': {
-      $tomcat_version = '8.0.26'
+      $tomcat_version = '8.0.39'
       $catalina_dir = '/opt/apache-tomcat8'
       $tomcat_other_versions = [ '6', '7']
     }
