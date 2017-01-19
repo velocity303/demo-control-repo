777caca8bcc60b72603273fee7c6298ea666515e
James E. Jones <james.jones@puppet.com>
2017-01-18 23:01:04 -0600

    update jenkins class

--- site/profile/manifests/app/jenkins.pp
+++ site/profile/manifests/app/jenkins.pp
@@ -2,7 +2,7 @@ class profile::app::jenkins (
   String $jenkins_version      = 'latest',
   String $tomcat_major_version = '7',
 ) {
-  include ::profile::firewall
+  #  include ::profile::firewall
 
   case $tomcat_major_version {
     '6': {
