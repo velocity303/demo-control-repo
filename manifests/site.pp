4a98c23f4634fbb562a15ea18f8856b5dc5839e1
James E. Jones <james.jones@puppet.com>
2017-01-25 13:52:19 -0600

    update to limit firewall to only linux nodes

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,18 +4,22 @@ Package {
   allow_virtual => true,
 }
 
-Firewall {
-  before  => Class['profile::firewall::post'],
-  require => Class['profile::firewall::pre'],
+if $::kernel == 'Linux' {
+
+  Firewall {
+    before  => Class['profile::firewall::post'],
+    require => Class['profile::firewall::pre'],
+  }
+
+  class { ['profile::firewall::pre', 'profile::firewall::post']: }
+
+  resources { 'firewall':
+    purge => true,
+  }
+
+  include firewall
+
 }
-
-class { ['profile::firewall::pre', 'profile::firewall::post']: }
-
-resources { 'firewall':
-  purge => true,
-}
-
-include firewall
 
 node default {
   #  if !empty( $trusted['extensions']['pp_role'] ) {
