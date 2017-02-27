9cfbb9d78d7f7823a07371eb7d9a67c8e4e05961
James E. Jones <james.jones@puppet.com>
2017-02-27 13:55:08 -0600

    update client to match datacenter of nodes

--- site/profile/manifests/inf/consul/client.pp
+++ site/profile/manifests/inf/consul/client.pp
@@ -4,7 +4,7 @@ class profile::inf::consul::client {
     class { '::consul':
       config_hash    => {
         'data_dir'   => '/opt/consul',
-        'datacenter' => $::location,
+        'datacenter' => 'vagrant',
         'log_level'  => 'INFO',
         'node_name'  => $::name,
         'retry_join' => [hiera('consul_node_ip')],
