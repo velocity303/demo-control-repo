bfd04b9db4e8167371876e60fd2e08383ec1e362
James E. Jones <james.jones@puppet.com>
2017-02-27 13:41:40 -0600

    update client configuration

--- site/profile/manifests/inf/consul/client.pp
+++ site/profile/manifests/inf/consul/client.pp
@@ -1,11 +1,13 @@
 class profile::inf::consul::client {
-  class { '::consul':
-    config_hash    => {
-      'data_dir'   => '/opt/consul',
-      'datacenter' => $::location,
-      'log_level'  => 'INFO',
-      'node_name'  => $::name,
-      'retry_join' => [hiera('consul_node_ip')],
+  if trusted['extensions']['pp_role'] != 'consul' {
+    class { '::consul':
+      config_hash    => {
+        'data_dir'   => '/opt/consul',
+        'datacenter' => $::location,
+        'log_level'  => 'INFO',
+        'node_name'  => $::name,
+        'retry_join' => [hiera('consul_node_ip')],
+      }
     }
   }
 }
