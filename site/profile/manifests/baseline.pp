9c4f86d505a786e4069ddd18c363e2fa9dff49af
James E. Jones <james.jones@puppet.com>
2017-02-27 02:05:25 -0600

    update to remove selinux from baseline

--- site/profile/manifests/baseline.pp
+++ site/profile/manifests/baseline.pp
@@ -7,7 +7,7 @@ class profile::baseline {
     include profile::baseline::ntp
     include profile::baseline::path
     include profile::puppet::agent
-    include profile::baseline::selinux
+    #include profile::baseline::selinux
   }
   elsif $::kernel == 'windows' {
     include profile::baseline::winpackages
