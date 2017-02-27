795cf6f538bc040925af596ad667534350a29417
James E. Jones <james.jones@puppet.com>
2017-02-27 13:43:23 -0600

    ensure unzip package is available for consul install

--- site/profile/manifests/inf/consul/client.pp
+++ site/profile/manifests/inf/consul/client.pp
@@ -1,5 +1,6 @@
 class profile::inf::consul::client {
   if trusted['extensions']['pp_role'] != 'consul' {
+    ensure_packages('unzip')
     class { '::consul':
       config_hash    => {
         'data_dir'   => '/opt/consul',
@@ -8,6 +9,7 @@ class profile::inf::consul::client {
         'node_name'  => $::name,
         'retry_join' => [hiera('consul_node_ip')],
       }
+      require        => Package['unzip'],
     }
   }
 }
