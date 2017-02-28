898bea7ad7423ba6d21e60fdc44d0fdd5ba69486
James E. Jones <james.jones@puppet.com>
2017-02-27 19:18:12 -0600

    update consul backend to force http

--- site/profile/manifests/inf/vault.pp
+++ site/profile/manifests/inf/vault.pp
@@ -1,17 +1,15 @@
 class profile::inf::vault {
   class { '::vault':
       backend       => {
-        'consul' => {}
+        'consul'    => {
+          'scheme' => 'http',
+        }
       },
       listener      => {
         'tcp' => {
           'address'     => "${::fqdn}:8200",
           'tls_disable' => '1',
         }
-      },
-      telemetry     => {
-        'statsite_address' => "${::ipaddress}:8125",
-        'disable_hostname' => true,
       },
       disable_mlock => true,
       download_url  => 'https://releases.hashicorp.com/vault/0.6.5/vault_0.6.5_linux_amd64.zip'
