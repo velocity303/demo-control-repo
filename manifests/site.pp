e7f063c1fc57e4d506f4d221d36114e5964335ee
James E. Jones <james.jones@puppet.com>
2017-03-02 19:44:31 -0600

    update to include simple test with notify resource

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,6 +25,8 @@ elsif $::kernel == 'windows' {
   Package { provider => chocolatey, }
 }
 
+notify { hiera('foo') }
+
 node default {
   #  if !empty( $trusted['extensions']['pp_role'] ) {
   #  include "role::${trusted['extensions']['pp_role']}"
