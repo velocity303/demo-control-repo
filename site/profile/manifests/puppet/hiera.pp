f9bc32147614f6aa83fe5ea14c5ad5fdc5e08350
James E. Jones <james.jones@puppet.com>
2017-03-02 19:32:51 -0600

    add vault as hiera backend

--- site/profile/manifests/puppet/hiera.pp
+++ site/profile/manifests/puppet/hiera.pp
@@ -2,8 +2,8 @@ class profile::puppet::hiera {
   #Check if puppetserver service is defined, if so manage restart after updating hiera
   if defined(Service['pe-puppetserver']) {
     class {'::hiera':
-      backends        => ['yaml','eyaml'],
-      #backend_options => {
+      backends        => ['yaml','eyaml', 'vault'],
+      backend_options => {
       #  'http' => {
       #    'host'          => 'jenkins01.infrastructure.lab',
       #    'port'          => 8080,
@@ -12,7 +12,11 @@ class profile::puppet::hiera {
       #    'failure'       => 'graceful',
       #    'paths'         => ['/hiera/lookup?scope=%{::trusted.certname}&key=%{key}', '/hiera/lookup?scope=%{::virtual}&key=%{key}','/hiera/lookup?scope=%{::environment}&key=%{key}'],
       #  }
-      #},
+         'vault'   => {
+           'addr'  => 'http://127.0.0.1:8200',
+           'token' => 'c41f9afc-4b44-da8b-7430-921fa4fe16f8',
+         }
+      },
       hierarchy       => [
         'secure',
         'nodes/%{hostname}',
@@ -31,8 +35,8 @@ class profile::puppet::hiera {
   }
   else {
     class {'::hiera':
-      backends        => ['yaml','eyaml'],
-      #backend_options => {
+      backends        => ['yaml','eyaml', 'vault'],
+      backend_options => {
       #  'http' => {
       #    'host'          => 'jenkins01.infrastructure.lab',
       #    'port'          => 8080,
@@ -41,7 +45,11 @@ class profile::puppet::hiera {
       #    'failure'       => 'graceful',
       #    'paths'         => ['/hiera/lookup?scope=%{::trusted.certname}&key=%{key}', '/hiera/lookup?scope=%{::virtual}&key=%{key}','/hiera/lookup?scope=%{::environment}&key=%{key}'],
       #  }
-      #},
+         'vault'   => {
+           'addr'  => 'http://127.0.0.1:8200',
+           'token' => 'c41f9afc-4b44-da8b-7430-921fa4fe16f8',
+         }
+      },
       hierarchy       => [
         'secure',
         'nodes/%{hostname}',
