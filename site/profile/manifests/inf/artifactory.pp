8f97bff390ad51ff799a194d496acd68b150d432
James E. Jones <james.jones@puppet.com>
2017-03-21 14:08:17 -0500

    update port for artifactory

--- site/profile/manifests/inf/artifactory.pp
+++ site/profile/manifests/inf/artifactory.pp
@@ -1,7 +1,7 @@
 class profile::inf::artifactory {
   include artifactory
   firewall { '100 allow artifactory through the firewall':
-    dport  => ['8080'],
+    dport  => ['8081'],
     proto  => tcp,
     action => accept,
   }
