0c5db366e74ea688fa17a086d25d4103fafb217d
James E. Jones <james.jones@puppet.com>
2017-04-16 22:16:32 -0500

    update hiera file

--- site/profile/manifests/puppet/hiera.pp
+++ site/profile/manifests/puppet/hiera.pp
@@ -2,16 +2,16 @@ class profile::puppet::hiera {
   #Check if puppetserver service is defined, if so manage restart after updating hiera
   if defined(Service['pe-puppetserver']) {
     class {'::hiera':
-      #backends        => ['yaml','eyaml', 'vault'],
-      #backend_options => {
-      #  'http' => {
-      #    'host'          => 'jenkins01.infrastructure.lab',
-      #    'port'          => 8080,
-      #    'output'        => 'json',
-      #    'cache_timeout' => 10,
-      #    'failure'       => 'graceful',
-      #    'paths'         => ['/hiera/lookup?scope=%{::trusted.certname}&key=%{key}', '/hiera/lookup?scope=%{::virtual}&key=%{key}','/hiera/lookup?scope=%{::environment}&key=%{key}'],
-      #  }
+      backends        => ['yaml','eyaml', 'http'],
+      backend_options => {
+        'http' => {
+          'host'          => 'jenkins01.infrastructure.lab',
+          'port'          => 8080,
+          'output'        => 'json',
+          'cache_timeout' => 10,
+          'failure'       => 'graceful',
+          'paths'         => ['/hiera/lookup?scope=%{::trusted.certname}&key=%{key}', '/hiera/lookup?scope=%{::virtual}&key=%{key}','/hiera/lookup?scope=%{::environment}&key=%{key}'],
+        }
       #   'vault'                    => {
       #     'addr'                   => 'http://127.0.0.1:8200',
       #     'token'                  => 'c41f9afc-4b44-da8b-7430-921fa4fe16f8',
@@ -39,16 +39,16 @@ class profile::puppet::hiera {
   }
   else {
     class {'::hiera':
-      #backends        => ['yaml','eyaml', 'vault'],
-      #backend_options => {
-      #  'http' => {
-      #    'host'          => 'jenkins01.infrastructure.lab',
-      #    'port'          => 8080,
-      #    'output'        => 'json',
-      #    'cache_timeout' => 10,
-      #    'failure'       => 'graceful',
-      #    'paths'         => ['/hiera/lookup?scope=%{::trusted.certname}&key=%{key}', '/hiera/lookup?scope=%{::virtual}&key=%{key}','/hiera/lookup?scope=%{::environment}&key=%{key}'],
-      #  }
+      backends        => ['yaml','eyaml', 'http'],
+      backend_options => {
+        'http' => {
+          'host'          => 'jenkins01.infrastructure.lab',
+          'port'          => 8080,
+          'output'        => 'json',
+          'cache_timeout' => 10,
+          'failure'       => 'graceful',
+          'paths'         => ['/hiera/lookup?scope=%{::trusted.certname}&key=%{key}', '/hiera/lookup?scope=%{::virtual}&key=%{key}','/hiera/lookup?scope=%{::environment}&key=%{key}'],
+        }
       #   'vault'   => {
       #     'addr'  => 'http://127.0.0.1:8200',
       #     'token' => 'c41f9afc-4b44-da8b-7430-921fa4fe16f8',
