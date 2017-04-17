53f01376bec0f5b31a8c5c6a3f7f5b042a454487
James E. Jones <james.jones@puppet.com>
2017-04-16 23:40:28 -0500

    update to remove hiera to convert to hiera 5

--- site/profile/manifests/puppet/master.pp
+++ site/profile/manifests/puppet/master.pp
@@ -1,5 +1,5 @@
 class profile::puppet::master {
-  include profile::puppet::hiera
+  #  include profile::puppet::hiera
   include profile::puppet::agent
   include profile::puppet::consoleusers
   include profile::puppet::autosign
