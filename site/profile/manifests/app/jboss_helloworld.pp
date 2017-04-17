6f8cd6e19e9b922f37cf9da611b733b97e964dca
James E. Jones <james.jones@puppet.com>
2017-04-17 03:30:33 -0500

    add jboss middleware class

--- site/profile/manifests/app/jboss_helloworld.pp
+++ site/profile/manifests/app/jboss_helloworld.pp
@@ -1,18 +1,8 @@
 class profile::app::jboss_helloworld (
   $source = 'https://s3-us-west-2.amazonaws.com/tseteam/files/jboss-helloworld.war'
 ) {
-
-  class { 'java':
-    distribution => 'jdk',
-  }
-
+  include profile::middleware::jboss
   wildfly::deployment { 'jboss-helloworld.war':
     source    => $source,
-  }
-
-  firewall { '100 allow jboss access':
-    dport  => [8080],
-    proto  => tcp,
-    action => accept,
   }
 }
---
+++ site/profile/manifests/middleware/jboss.pp
@@ -0,0 +1,23 @@
+class profile::middleware::wildfly (
+  $version = '8.0.0',
+  $source = 'http://download.jboss.org/wildfly/8.0.0.Final/wildfly-8.0.0.Final.tar.gz'
+) {
+
+  class { 'java':
+    version => latest,
+  }
+
+  class { '::wildfly':
+    install_source => $source,
+    version        => $version,
+    require        => Class['java'],
+    java_home      => $::java_default_home,
+  }
+
+  firewall { '100 allow connections to wildfly':
+    proto   => 'tcp',
+    dport   => '8080',
+    action  => 'accept',
+  }
+
+}
