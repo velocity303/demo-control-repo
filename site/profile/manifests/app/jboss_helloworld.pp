686fb9fdddeb63af4ba990ca842e19c856eae5ea
James E. Jones <james.jones@puppet.com>
2017-04-17 03:18:46 -0500

    update to include main wildfly class

--- site/profile/manifests/app/jboss_helloworld.pp
+++ site/profile/manifests/app/jboss_helloworld.pp
@@ -1,10 +1,12 @@
 class profile::app::jboss_helloworld (
   $source = 'https://s3-us-west-2.amazonaws.com/tseteam/files/jboss-helloworld.war'
 ) {
+
   class { 'java':
     distribution => 'jdk',
   }->
 
+  include wildfly
   wildfly::deployment { 'jboss-helloworld.war':
     source    => $source,
   }
