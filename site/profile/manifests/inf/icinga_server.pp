5174a099bde98f6a18a7f92902ab67a4d5ce8284
James E. Jones <james.jones@puppet.com>
2017-04-22 10:26:08 -0700

    update icinga2 profile for ordering issues

--- site/profile/manifests/inf/icinga_server.pp
+++ site/profile/manifests/inf/icinga_server.pp
@@ -67,9 +67,9 @@ class profile::inf::icinga_server {
     web_db_user         => 'icingaweb2',
     web_db_pass         => 'icinga2',
     web_db_port         => '3306',
-    require             => [ Class['profile::baseline::epel'], Mysql::Db['icingaweb2'], Class['icinga2::repo'], File['/etc/httpd/conf/httpd.conf'] ],
+    require             => [ Class['profile::baseline::epel'], Mysql::Db['icingaweb2'], Class['icinga2::repo']],
   }
-
+  File['/etc/httpd/conf/httpd.conf'] -> Exec['syntax verification for icingaweb2']
   Yumrepo['icinga-stable-release'] -> Package['icingaweb2']
   contain ::icingaweb2::mod::monitoring
 
