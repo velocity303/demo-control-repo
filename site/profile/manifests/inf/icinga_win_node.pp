cfc6bfe3a4b5f7707fef00f5b7352ec48f0a0f52
James E. Jones <james.jones@puppet.com>
2017-04-22 10:42:18 -0700

    update namespace for icinga win node

--- site/profile/manifests/inf/icinga_win_node.pp
+++ site/profile/manifests/inf/icinga_win_node.pp
@@ -1,4 +1,4 @@
-class profile::icinga_win_node {
+class profile::inf::icinga_win_node {
   class { 'nsclient':
     allowed_hosts           => ['192.168.1.0/24', '192.168.2.0/24', '192.168.3.0/24'],
     package_source_location => 'https://github.com/mickem/nscp/releases/download/0.4.1.90',
