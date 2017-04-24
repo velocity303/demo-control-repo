f7336221f8d42da6dbf82195cd32dbddbce4f09e
James E. Jones <james.jones@puppet.com>
2017-04-24 18:35:25 -0500

    change location of firewall rules for puppetmaster role

--- site/profile/manifests/puppet/master.pp
+++ site/profile/manifests/puppet/master.pp
@@ -9,4 +9,22 @@ class profile::puppet::master {
     projectname     => 'My Roles and Profiles',
     project_version => '1.0',
   }
+
+  firewall { '100 allow Puppet master access':
+    dport  => '8140',
+    proto  => tcp,
+    action => accept,
+  }
+
+  firewall { '100 allow ActiveMQ MCollective access':
+    dport  => '61613',
+    proto  => tcp,
+    action => accept,
+  }
+  firewall { '100 allow PE Specific access':
+    dport  => ['443','8142', '4433', '8170', '8143'],
+    proto  => tcp,
+    action => accept,
+  }
+
 }
--- site/role/manifests/puppetmaster.pp
+++ site/role/manifests/puppetmaster.pp
@@ -1,7 +1,6 @@
 # This is a role for the management of the puppet master
 class role::puppetmaster {
   include profile::baseline
-  include profile::firewall::puppetmaster
   include profile::puppet::master
   #  include profile::inf::vault
 }
