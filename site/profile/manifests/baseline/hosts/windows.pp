4b95483f093d5857b03c5917398107cad571f6c6
James E. Jones <james.jones@puppet.com>
2017-04-24 17:57:17 -0500

    try hosts module on windows

--- site/profile/manifests/baseline/hosts/windows.pp
+++ site/profile/manifests/baseline/hosts/windows.pp
@@ -1,3 +1,9 @@
 class profile::baseline::hosts::windows {
   # To be defined - this should include everything to define the hosts file on a Windows system.
+  class  { '::hosts':
+    collect_all           => true,
+    purge_hosts           => true,
+    enable_ipv6_localhost => false,
+  }
+
 }
