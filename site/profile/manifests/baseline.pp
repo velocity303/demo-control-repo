b11a5164bb9abd81c91aac900d7901bd947474ae
James E. Jones <james.jones@puppet.com>
2017-04-24 18:03:10 -0500

    update ordering for host entries

--- site/profile/manifests/baseline.pp
+++ site/profile/manifests/baseline.pp
@@ -1,17 +1,20 @@
 class profile::baseline {
 
-    include profile::baseline::hosts
-    include profile::baseline::users
-    include profile::baseline::packages
-    include profile::baseline::banner
-    include profile::baseline::remote_access
-    include profile::baseline::firewall
-    include profile::baseline::security
-    include profile::baseline::time
-    include profile::baseline::path
-    include profile::baseline::puppet_agent
-    include profile::baseline::monitoring
-    include profile::baseline::misc
+  File{
+    require => Host['puppet'],
+  }
 
+  include profile::baseline::hosts
+  include profile::baseline::users
+  include profile::baseline::packages
+  include profile::baseline::banner
+  include profile::baseline::remote_access
+  include profile::baseline::firewall
+  include profile::baseline::security
+  include profile::baseline::time
+  include profile::baseline::path
+  include profile::baseline::puppet_agent
+  include profile::baseline::monitoring
+  include profile::baseline::misc
 }
 
