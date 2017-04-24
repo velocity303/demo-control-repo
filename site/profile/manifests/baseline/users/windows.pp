501310379b8981cf156b84e5b305ba2cf340c0aa
James E. Jones <james.jones@puppet.com>
2017-04-24 17:18:32 -0500

    update windows baseline for users

--- site/profile/manifests/baseline/users/windows.pp
+++ site/profile/manifests/baseline/users/windows.pp
@@ -1,7 +1,7 @@
 class profile::baseline::users::windows{
   user { 'Administrator':
     ensure   => present,
-    password => 'Password1',
+    password => hiera('local_admin_pass'),
   }
   reboot { 'changing administrator':
     subscribe => User['Administrator'],
