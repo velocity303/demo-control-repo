b37354d708d0c5cc1c4088fcb86d517b801d0d72
James E. Jones <james.jones@puppet.com>
2017-04-24 19:17:40 -0500

    update windows firewall rules for baseline

--- site/profile/manifests/baseline/firewall/windows.pp
+++ site/profile/manifests/baseline/firewall/windows.pp
@@ -1,3 +1,29 @@
 class profile::baseline::firewall::windows {
-  # A baseline profile for determining how my system should be configured for windows in a firewall scenario.
+
+  # Allow WinRM
+  windows_firewall::exception { 'WINRM':
+    ensure       => present,
+    direction    => 'in',
+    action       => 'Allow',
+    enabled      => 'yes',
+    protocol     => 'TCP',
+    local_port   => '5985',
+    remote_port  => 'any',
+    display_name => 'Windows Remote Management HTTP-In',
+    description  => 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5985]',
+  }
+
+  # Allow WinRDP
+  windows_firewall::exception { 'RDP':
+    ensure       => present,
+    direction    => 'in',
+    action       => 'Allow',
+    enabled      => 'yes',
+    protocol     => 'TCP',
+    local_port   => '3389',
+    remote_port  => 'any',
+    display_name => 'Remote Desktop - User Mode (TCP-In)',
+    description  => 'Inbound rule for Windows Remote Desktop service to allow RDP traffic. [TCP 3389]',
+  }
+
 }
