7719c328eac37b54a69fb16b274535c804901022
James E. Jones <james.jones@puppet.com>
2017-04-24 22:49:14 -0500

    update linux compliance profiles

--- site/profile/manifests/baseline/security/linux.pp
+++ site/profile/manifests/baseline/security/linux.pp
@@ -1,4 +1,7 @@
 class profile::baseline::security::linux {
   include profile::baseline::security::linux::selinux
   include profile::baseline::security::linux::sudo
+  include profile::baseline::security::linux::rhel_svc_denied
+  include profile::baseline::security::linux::rhel_pkg_denied
+  include profile::baseline::security::linux::rhel_network
 }
---
+++ site/profile/manifests/baseline/security/linux/rhel_pkg_denied.pp
@@ -0,0 +1,10 @@
+class profile::baseline::security::linux::rhel_pkg_denied {
+
+  $removed = [ 'telnet-server', 'rsh-server', 'tftp-server', 'vsftpd' ]
+
+  # Passing above array into the package function
+  package { $removed:
+    ensure => absent,
+  }
+
+}
