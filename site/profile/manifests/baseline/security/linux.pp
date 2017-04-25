3dabacb41cb233074174ad37d3a846bc3bd1dc34
James E. Jones <james.jones@puppet.com>
2017-04-25 02:03:46 -0500

    remove selinux profile code

--- site/profile/manifests/baseline/security/linux.pp
+++ site/profile/manifests/baseline/security/linux.pp
@@ -1,5 +1,5 @@
 class profile::baseline::security::linux {
-  include profile::baseline::security::linux::selinux
+  #  include profile::baseline::security::linux::selinux
   include profile::baseline::security::linux::sudo
   include profile::baseline::security::linux::rhel_svc_denied
   include profile::baseline::security::linux::rhel_pkg_denied
