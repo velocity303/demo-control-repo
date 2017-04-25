c6b8d7ee67cf5eb19da31a834dc5b1d81705037b
James E. Jones <james.jones@puppet.com>
2017-04-24 23:23:43 -0500

    update app_orch_settings for windows and linux use

--- site/profile/manifests/puppet/app_orch_settings.pp
+++ site/profile/manifests/puppet/app_orch_settings.pp
@@ -1,9 +1,15 @@
 class profile::puppet::app_orch_settings (
   $setting = false,
 ){
+  if $::kernel == 'windows' {
+    $config_file = 'C:\ProgramData\PuppetLabs\puppet\etc\puppet.conf'
+  }
+  else {
+    $config_file = '/etc/puppetlabs/puppet/puppet.conf'
+  }
   ini_setting { 'use_cached_catalog':
     ensure  => present,
-    path    => "${::confdir}/puppet.conf",
+    path    => $config_file,
     section => 'agent',
     setting => 'use_cached_catalog',
     value   => $setting,
