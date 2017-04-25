a0d708d37aa7593224fc0affbf78178be5d631cc
James E. Jones <james.jones@puppet.com>
2017-04-24 21:32:58 -0500

    update windows baseline

--- site/profile/manifests/baseline/security/windows.pp
+++ site/profile/manifests/baseline/security/windows.pp
@@ -1,3 +1,5 @@
 class profile::baseline::security::windows {
   # A stub for windows security baseline settings
+  include profile::baseline::security::windows::cis
+  include profile::baseline::security::windows::hipaa
 }
