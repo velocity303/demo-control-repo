eacc8c7da746ed4cdff823ca4328b4b9f7fb2131
James E. Jones <james.jones@puppet.com>
2017-04-28 16:47:41 -0500

    update to include app in definition

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,6 +4,18 @@ Package {
   allow_virtual => true,
 }
 
+site {
+  consul_cluster { 'infrastructure':
+    datacenter                            => "infrastructure",
+    nodes                                 => {
+      Node['consul01.infrastructure.lab'] => Consul_cluster::Head['infrastructure'],
+      Node['consul02.infrastructure.lab'] => Consul_cluster::Node['infrastructure'],
+      Node['consul03.infrastructure.lab'] => Consul_cluster::Node['infrastructure']
+    },
+  }
+
+}
+
 node default {
   #  if !empty( $trusted['extensions']['pp_role'] ) {
   #  include "role::${trusted['extensions']['pp_role']}"
