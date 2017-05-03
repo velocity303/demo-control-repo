1b5aedb9783598ca0fcb3a30b9f260bfc690dfdb
James E. Jones <james.jones@puppet.com>
2017-05-03 09:35:26 -0500

    update ad profile for linting

--- site/profile/manifests/inf/ad.pp
+++ site/profile/manifests/inf/ad.pp
@@ -11,24 +11,32 @@ class profile::inf::ad (
   dsc_windowsfeature { 'ad-domain-services':
     ensure   => present,
     dsc_name => 'ad-domain-services',
-  } ->
+  }
 
-  dsc_xaddomain { $domain_name:
+  -> dsc_xaddomain { $domain_name:
     ensure                            => present,
     dsc_domainname                    => $domain_name,
-    dsc_domainadministratorcredential =>  { user     => 'Administrator',
- password => $domain_admin_pass },
-    dsc_safemodeadministratorpassword => { user     => 'Administrator',
- password => $domain_admin_pass },
+    dsc_domainadministratorcredential =>  {
+      'user'     => 'Administrator',
+      'password' => $domain_admin_pass,
+    },
+    dsc_safemodeadministratorpassword => {
+      'user'     => 'Administrator',
+      'password' => $domain_admin_pass
+    },
     notify                            => Reboot['dsc_reboot'],
   }
 
   dsc_xaddomaincontroller { 'lab':
     dsc_domainname                    => $domain_name,
-    dsc_domainadministratorcredential =>  { user     => 'Administrator',
- password => $domain_admin_pass },
-    dsc_safemodeadministratorpassword => { user     => 'Administrator',
- password => $domain_admin_pass },
+    dsc_domainadministratorcredential =>  {
+      'user'     => 'Administrator',
+      'password' => $domain_admin_pass
+    },
+    dsc_safemodeadministratorpassword => {
+      'user'     => 'Administrator',
+      'password' => $domain_admin_pass
+    },
     notify                            => Reboot['dsc_reboot'],
   }
 }
