c614e04008f6615e16d4fa5ed0929c48ffec8b5e
James E. Jones <james.jones@puppet.com>
2017-05-03 09:20:01 -0500

    temporarily remove consul from site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,17 +4,17 @@ Package {
   allow_virtual => true,
 }
 
-site {
-  consul_cluster { 'infrastructure':
-    datacenter                            => "infrastructure",
-    nodes                                 => {
-      Node['consul01.infrastructure.lab'] => Consul_cluster::Head['infrastructure'],
-      Node['consul02.infrastructure.lab'] => Consul_cluster::Node['infrastructure-0'],
-      Node['consul03.infrastructure.lab'] => Consul_cluster::Node['infrastructure-1']
-    },
-  }
-
-}
+#site {
+#  consul_cluster { 'infrastructure':
+#    datacenter                            => "infrastructure",
+#    nodes                                 => {
+#      Node['consul01.infrastructure.lab'] => Consul_cluster::Head['infrastructure'],
+#      Node['consul02.infrastructure.lab'] => Consul_cluster::Node['infrastructure-0'],
+#      Node['consul03.infrastructure.lab'] => Consul_cluster::Node['infrastructure-1']
+#    },
+#  }
+#
+#}
 
 node default {
   #  if !empty( $trusted['extensions']['pp_role'] ) {
