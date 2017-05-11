f61469f25c4bfd189386f43e94bc2f1e01be7fb8
James E. Jones <james.jones@puppet.com>
2017-05-11 13:54:52 -0500

    update docker host definition

--- site/profile/manifests/inf/docker_host.pp
+++ site/profile/manifests/inf/docker_host.pp
@@ -1,3 +1,8 @@
 class profile::inf::docker_host {
   include docker
+  docker::run { 'jenkins':
+    image           => 'jenkins',
+    ports           => ['8080', '50000'],
+    expose          => ['8080', '50000'],
+  }
 }
