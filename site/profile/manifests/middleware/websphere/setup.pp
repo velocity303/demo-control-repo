7a96989a6bf91c82dbc7c575d879fbc7db286f8f
James E. Jones <james.jones@puppet.com>
2017-06-05 12:23:41 -0500

    update setup

--- site/profile/manifests/middleware/websphere/setup.pp
+++ site/profile/manifests/middleware/websphere/setup.pp
@@ -10,4 +10,19 @@ class profile::middleware::websphere::setup {
     creates => '/opt/ibm_files/agent.installer.linux.gtk.x86_64_1.6.0.20120831_1216.zip',
   }
 
+  class { 'websphere_application_server':
+    user         => 'webadmin',
+    group        => 'webadmin',
+    manage_user  => true,
+    manage_group => true,
+  }
+
+  websphere_application_server::instance { 'WebSphere85':
+    target       => '/opt/IBM/WebSphere/AppServer',
+    package      => 'com.ibm.websphere.NDTRIAL.v85',
+    version      => '8.5.5000.20130514_1044',
+    profile_base => '/opt/IBM/WebSphere/AppServer/profiles',
+    repository   => '/opt/ibm_files/was.repo.8550.basetrial/repository.config',
+  }
+
 }
