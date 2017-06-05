c209cadcffdcb5782f92b99143861f80d8c0fbe8
James E. Jones <james.jones@puppet.com>
2017-06-05 11:59:07 -0500

    update setup class

--- site/profile/manifests/middleware/websphere/setup.pp
+++ site/profile/manifests/middleware/websphere/setup.pp
@@ -5,8 +5,9 @@ class profile::middleware::websphere::setup {
   }->
 
   staging::deploy { 'websphere_bundle.tar.gz':
-    source => 'http://fileserver.infrastructure.lab/websphere_bundle.tar.gz',
-    target => '/opt/ibm_files',
+    source  => 'http://fileserver.infrastructure.lab/websphere_bundle.tar.gz',
+    target  => '/opt/ibm_files',
+    creates => '/opt/ibm_files/agent.installer.linux.gtk.x86_64_1.6.0.20120831_1216.zip',
   }
 
 }
