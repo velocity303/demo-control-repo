5ba7929255ab22c88f888560e83c8f197758e164
James E. Jones <james.jones@puppet.com>
2017-07-05 08:43:47 -0500

    add james rule during defined window

--- site/profile/manifests/baseline/security/linux/sudo.pp
+++ site/profile/manifests/baseline/security/linux/sudo.pp
@@ -1,14 +1,14 @@
 class profile::baseline::security::linux::sudo (
   $tz = "-06:00",
   $window_wday  = { start => 'Wednesday', end => 'Thursday' },
-  $window_time = { start  => '09:00', end => '12:00' },
+  $window_time = { start  => '08:00', end => '12:00' },
   $window_type = 'window',
 ) {
   class { 'sudo':
     purge => true,
   }
 
-  #  $enforce_sudo_rules = change_window($tz, $window_type, $window_wday, $window_time)
+  $enforce_sudo_rules = change_window($tz, $window_type, $window_wday, $window_time)
 
   group {'wheel':
     ensure => present,
@@ -22,9 +22,9 @@ class profile::baseline::security::linux::sudo (
     }
   }
 
-  #if $enforce_sudo_rules == 'true' {
-  #  sudo::conf { 'add james rule':
-  #    content => 'james ALL=(ALL) NOPASSWD:ALL',
-  #  }
-  #}
+  if $enforce_sudo_rules == 'true' {
+    sudo::conf { 'add james rule':
+      content => 'james ALL=(ALL) NOPASSWD:ALL',
+    }
+  }
 }
