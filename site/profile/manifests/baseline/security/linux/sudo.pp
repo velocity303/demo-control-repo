7d9b1d812d3370f6e8c21aecd0e4ea8b9a245952
James E. Jones <james.jones@puppet.com>
2017-07-05 07:22:34 -0500

    update for sudo example with change window

--- site/profile/manifests/baseline/security/linux/sudo.pp
+++ site/profile/manifests/baseline/security/linux/sudo.pp
@@ -1,5 +1,12 @@
 class profile::baseline::security::linux::sudo {
+  $tz = "-06:00"
+  $window_wday  = { start => 'Wednesday', end => 'Thursday' }
+  $window_time = { start  => '10:00', end => '12:00' }
+  $window_type = 'window'
+) {
   include sudo
+  $rule_enforced = change_window($tz, $window_type, $window_wday, $window_time)
+
   group {'wheel':
     ensure => present,
   }
@@ -9,6 +16,11 @@ class profile::baseline::security::linux::sudo {
   if $::virtual == 'virtualbox' {
     sudo::conf { 'vagrant':
       content => 'vagrant ALL=(ALL) NOPASSWD:ALL',
+    }
+  }
+  if $rule_enforced == 'true' {
+    sudo::conf { 'add james rule':
+      content => 'james ALL=(ALL) NOPASSWD:ALL',
     }
   }
 }
