696e2d9df5ba093c745cd39be8fbe316c1dde1e0
James E. Jones <james.jones@puppet.com>
2017-10-03 01:00:15 -0500

    update lookups for windows admin pass

--- site/profile/manifests/baseline/users/windows.pp
+++ site/profile/manifests/baseline/users/windows.pp
@@ -1,7 +1,7 @@
 class profile::baseline::users::windows{
   user { 'Administrator':
     ensure   => present,
-    password => hiera('local_admin_pass'),
+    password => lookup('local_admin_pass'),
   }
   reboot { 'changing administrator':
     subscribe => User['Administrator'],
